5e2309acd84d6ebf4cb00cd29ce5543cfa1f53ad
atmasark <moroatte@gmail.com>
2020-01-17 20:31:21 +0200

    minor change to the styles of index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ interface IndexPageProps {
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
